afdfbe33808a6ff3063bd09b1518eca814e279fc
Aykut Beyaz <ayktbyz@gmail.com>
2024-03-21 18:42:47 +0200

    Initialize commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Thecodebaseio Case
---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
+
+buildscript {
+    extra.set("compileSdkVersion", 34)
+    extra.set("minSdkVersion", 26)
+    extra.set("targetSdkVersion", 34)
+}
